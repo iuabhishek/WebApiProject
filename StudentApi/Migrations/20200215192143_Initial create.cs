5766d3b180e9d208d0e7a2af90edb89c4d0316f7
Abhi <Abhi@DESKTOP-MQHE6RN>
2020-02-22 00:48:34 +0530

    web api

---
+++ StudentApi/Migrations/20200215192143_Initial create.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentApi.Migrations
+{
+    public partial class Initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Stu_id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Phone = table.Column<string>(nullable: false),
+                    City = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Age = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Stu_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ StudentApi/Model/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApi.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Stu_id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(100)")]
+        public string Name { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string City { get; set; }
+        [Required]
+        public int Age { get; set; }
+
+    }
+}
